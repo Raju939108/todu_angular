4f379255cdb9127482572fb516be50343049d603
Raju939108 <karumanchinagaraju08@gmail.com>
2026-08-26 18:00:59 +0530

    added the audio

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,39 +9,58 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('todo');
-  tasks_text:string=''
 
-  tasks:string[]= []
-  Completed:string[]=[]
+  tasks_text: string = '';
 
-  addText(event:any){
-      this.tasks_text=event.target.value;
+  tasks: string[] = [];
+
+  Completed: string[] = [];
+
+  addText(event: any) {
+    this.tasks_text = event.target.value;
   }
 
-  addTask(){
-    if(this.tasks_text==''){
-      alert('Please enter Task')
+  addTask() {
+
+    if (this.tasks_text == '') {
+
+      alert('Please enter Task');
+
+    } else {
+
+      this.tasks.push(this.tasks_text);
+
+      let v: any = document.getElementById('inp');
+
+      v.value = '';
+
+      this.tasks_text = '';
     }
-    else{
-      this.tasks.push(this.tasks_text)
-
-    let v:any = document.getElementById('inp');
-    v.value=''
-    this.tasks_text=''
-
-    }
-
-    
   }
 
   deleteTask(index: number) {
+
+    const audio = new Audio('delete-sound.mp3');
+
+    audio.play();
+
     this.tasks.splice(index, 1);
- }
+  }
 
- taskComplated(index: number) {
+  taskComplated(index: number) {
 
-    this.Completed.push(this.tasks[index]);
+    const audio = new Audio('dragon-sound.mp3');
 
-    this.tasks.splice(index, 1);}
+    audio.play();
+
+    setTimeout(() => {
+
+      this.Completed.push(this.tasks[index]);
+
+      this.tasks.splice(index, 1);
+
+    }, 1000);
+  }
 }
