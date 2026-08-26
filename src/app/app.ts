1f3463472341f82a90642bf2d3bda4c54ea0fdcd
Raju939108 <karumanchinagaraju08@gmail.com>
2026-08-26 15:38:57 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todo');
+  tasks_text:string=''
+
+  tasks:string[]= []
+
+  addText(event:any){
+      this.tasks_text=event.target.value;
+  }
+
+  addTask(){
+    if(this.tasks_text==''){
+      alert('Please enter Task')
+    }
+    else{
+      this.tasks.push(this.tasks_text)
+
+    let v:any = document.getElementById('inp');
+    v.value=''
+    this.tasks_text=''
+
+    }
+
+    
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+ }
 }
